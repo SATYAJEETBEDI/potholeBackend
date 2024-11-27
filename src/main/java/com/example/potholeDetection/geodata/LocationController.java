d378a65caaaab3dbde3c2953a4c18106eccbcd9f
Satyajeet Bedi <satyajeetbedi145@gmail.com>
2024-11-27 19:43:53 +0530

    deployment solved

--- src/main/java/com/example/potholeDetection/geodata/LocationController.java
+++ src/main/java/com/example/potholeDetection/geodata/LocationController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -27,7 +29,11 @@ public class LocationController {
     //     return locationService.googleDistance(location);
     // }
     
-
+    @GetMapping("/test")
+    public String getMethodName() {
+        return "deployed successfully";
+    }
+    
 
     @PostMapping("/")
     String create(@RequestBody Location location) {
