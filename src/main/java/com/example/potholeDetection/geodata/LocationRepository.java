f16d94d23c0ac731335b7f78fa1b7c6a0e2cffa1
Satyajeet Bedi <satyajeetbedi145@gmail.com>
2024-12-17 15:48:28 +0530

    remove functionality added

--- src/main/java/com/example/potholeDetection/geodata/LocationRepository.java
+++ src/main/java/com/example/potholeDetection/geodata/LocationRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LocationRepository extends JpaRepository<Location, Long>{
 
     Location findByLatitudeAndLongitude(double latitude, double longitude);
-    Location deleteByvalLocation(Location location);
+    Location deleteByLocation(Location location);
 }
